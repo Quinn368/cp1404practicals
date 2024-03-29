d9e588bada0fa599a68492f7b555365901378163
git <quinnsierra368@gmail.com>
2024-03-30 00:34:27 +0800

    Add prac_08 files

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,31 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+from kivy.properties import StringProperty
+
+NEW_COLOUR = (1, 0, 0, 1)  # RGBA for red
+ALTERNATIVE_COLOUR = (1, 0, 1, 1)  # RGBA for magenta
+
+
+class DynamicLabelsApp(App):
+    """MilesConverterApp is a Kivy App to demo dynamic label creation."""
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        """Construct main app."""
+        super().__init__(**kwargs)
+        self.names = ["Bob Brown", "Cat Cyan", "Oren Ochre"]
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Widgets"
+        self.root = Builder.load_file('dynamic_labels.kv')
+
+        for name in self.names:
+            name_label = Label(text=name)
+            self.root.ids.main.add_widget(name_label)
+
+        return self.root
+
+
+DynamicLabelsApp().run()
